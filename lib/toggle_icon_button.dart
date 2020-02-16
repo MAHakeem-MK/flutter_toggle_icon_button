d33f14ca62e1cc86e44e9636f9b2239b96942e3c
Muhammed Abdul Hakeem MK <dev.mahakeemmk@gmail.com>
2020-02-16 12:08:13 +0530

    toggle icon button widget implemented.

--- lib/toggle_icon_button.dart
+++ lib/toggle_icon_button.dart
@@ -1,7 +1,40 @@
 library toggle_icon_button;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class ToggleIconButton extends StatefulWidget {
+  final IconData icon1;
+  final IconData icon2;
+  final Function onPressed;
+  final TextDirection textDirection;
+  ToggleIconButton(
+      {@required IconData this.icon1,
+      @required IconData this.icon2,
+      TextDirection this.textDirection = TextDirection.ltr,
+      @required this.onPressed});
+  _ToggleIconButtonState createState() => _ToggleIconButtonState();
+}
+
+class _ToggleIconButtonState extends State<ToggleIconButton> {
+  IconData _currentIcon;
+
+  @override
+  void initState() {
+    _currentIcon = widget.icon1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      child: Material(
+          child: IconButton(icon: Icon(_currentIcon), onPressed: toggle)),
+      textDirection: widget.textDirection,
+    );
+  }
+
+  void toggle() {
+    widget.onPressed();
+    setState(() => _currentIcon =
+        (_currentIcon == widget.icon1) ? widget.icon2 : widget.icon1);
+  }
 }
