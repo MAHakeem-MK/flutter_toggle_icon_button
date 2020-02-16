58d29e6376bf37bea69d304d92e9d5eaed370520
Muhammed Abdul Hakeem MK <dev.mahakeemmk@gmail.com>
2020-02-16 12:08:37 +0530

    test for basic toggle icon button.

--- test/toggle_icon_button_test.dart
+++ test/toggle_icon_button_test.dart
@@ -1,13 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:flutter/material.dart';
 import 'package:toggle_icon_button/toggle_icon_button.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  testWidgets('default is icon 1', (WidgetTester tester) async {
+    await tester.pumpWidget(ToggleIconButton(
+        icon1: Icons.ac_unit, icon2: Icons.access_alarm, onPressed: () {}));
+    final toggleIconButtonWithIcon1 = find.byIcon(Icons.ac_unit);
+    final toggleIconButtonWithIcon2 = find.byIcon(Icons.access_alarm);
+    expect(toggleIconButtonWithIcon1, findsOneWidget);
+    expect(toggleIconButtonWithIcon2, findsNothing);
+  });
+
+  testWidgets('on click icon changes', (WidgetTester tester) async {
+    await tester.pumpWidget(ToggleIconButton(
+        icon1: Icons.ac_unit, icon2: Icons.access_alarm, onPressed: () {}));
+    final widget = find.byType(ToggleIconButton);
+    await tester.tap(widget);
+    final toggleIconButtonWithIcon2 = find.byIcon(Icons.access_alarm);
+    expect(toggleIconButtonWithIcon2, findsNothing);
   });
 }
